03c995f5e032c1c3abc6844c7aa4a1a7d65af6c2
ugrewer <uwe.grewer@gmx.com>
2025-11-12 16:12:16 +1000

    changed html_extra_path

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_extra_path = ['_APSIM_code', '_downloads']
+html_extra_path = ['_APSIM_code']
 
 # Custom CSS
 html_css_files = [
